f549e514c305e8476f3c9c0f8b0515cfc3941b13
Javier Quiroga <contact@javierquiroga.com>
2025-01-28 19:45:07 +0000

    Create function to render weather

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles.css';
 import getWeather from './getWeather';
 import getCity from './getCity';
+import weatherIcons from './icons';
 
 const Selector = (function () {
   return {
@@ -14,64 +15,51 @@ const Selector = (function () {
       0: {
         icon: document.getElementById('0-icon'),
         temp: document.getElementById('0-temp'),
-        conditions: document.getAnimations('0-conditions'),
+        conditions: document.getElementById('0-conditions'),
       },
       1: {
         icon: document.getElementById('1-icon'),
         temp: document.getElementById('1-temp'),
-        conditions: document.getAnimations('1-conditions'),
+        conditions: document.getElementById('1-conditions'),
       },
       2: {
         day: document.getElementById('2-day'),
         icon: document.getElementById('2-icon'),
         temp: document.getElementById('2-temp'),
-        conditions: document.getAnimations('2-conditions'),
+        conditions: document.getElementById('2-conditions'),
       },
       3: {
         day: document.getElementById('3-day'),
         icon: document.getElementById('3-icon'),
         temp: document.getElementById('3-temp'),
-        conditions: document.getAnimations('3-conditions'),
+        conditions: document.getElementById('3-conditions'),
       },
     },
   };
 })();
 
-function displayWeather(obj) {
-  // eslint-disable-next-line no-console
-  console.log(`
-        Location: ${obj.location}, ${obj.country}
-        Temperature: ${obj.currentConditions.temp}
-        Feels Like: ${obj.currentConditions.feelsLike}
-        Conditions: ${obj.currentConditions.conditions}
-        ${obj.currentConditions.icon}
+function renderWeather(obj) {
+  Selector.location.textContent = `${obj.location}, ${obj.country}`;
+  Selector.currentIcon.src = weatherIcons[obj.currentConditions.icon];
+  Selector.currentTemp.textContent = `${obj.currentConditions.temp}º`;
+  Selector.currentConditions.textContent = obj.currentConditions.conditions;
+  Selector.currentFeelsLike.textContent = `Feels like ${obj.currentConditions.feelsLike}º`;
 
-        Today
-        ${obj.days[0].date}
-        ${obj.days[0].conditions}
-        Max today: ${obj.days[0].tempMax}
-        Min today: ${obj.days[0].tempMin}
-        ${obj.days[0].icon}
+  Selector.days[0].icon.src = weatherIcons[obj.days[0].icon];
+  Selector.days[0].temp.textContent = `${obj.days[0].tempMin}º | ${obj.days[0].tempMax}º`;
+  Selector.days[0].conditions.textContent = obj.days[0].conditions;
 
-        Tomorrow
-        ${obj.days[1].date}
-        ${obj.days[1].conditions}
-        Max today: ${obj.days[1].tempMax}
-        Min today: ${obj.days[1].tempMin}
-        ${obj.days[1].icon}
+  Selector.days[1].icon.src = weatherIcons[obj.days[1].icon];
+  Selector.days[1].temp.textContent = `${obj.days[1].tempMin}º | ${obj.days[1].tempMax}º`;
+  Selector.days[1].conditions.textContent = obj.days[1].conditions;
 
-        ${obj.days[2].dayOfWeek}
-        ${obj.days[2].date}
-        ${obj.days[2].conditions}
-        Max today: ${obj.days[2].tempMax}
-        Min today: ${obj.days[2].tempMin}
-        ${obj.days[2].icon}
+  Selector.days[2].day.textContent = obj.days[2].dayOfWeek;
+  Selector.days[2].icon.src = weatherIcons[obj.days[2].icon];
+  Selector.days[2].temp.textContent = `${obj.days[2].tempMin}º | ${obj.days[2].tempMax}º`;
+  Selector.days[2].conditions.textContent = obj.days[2].conditions;
 
-        ${obj.days[3].dayOfWeek}
-        ${obj.days[3].date}
-        ${obj.days[3].conditions}
-        Max today: ${obj.days[3].tempMax}
-        Min today: ${obj.days[3].tempMin}
-        ${obj.days[3].icon}
-        `);
+  Selector.days[3].day.textContent = obj.days[3].dayOfWeek;
+  Selector.days[3].icon.src = weatherIcons[obj.days[3].icon];
+  Selector.days[3].temp.textContent = `${obj.days[3].tempMin}º | ${obj.days[3].tempMax}º`;
+  Selector.days[3].conditions.textContent = obj.days[3].conditions;
 }
