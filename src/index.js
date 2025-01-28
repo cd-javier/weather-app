a94e9e76f72c1ba9369ea5576f9662ad3932de2d
Javier Quiroga <contact@javierquiroga.com>
2025-01-28 16:14:10 +0000

    Create function to obtain user location

--- src/index.js
+++ src/index.js
@@ -94,3 +94,10 @@ function displayWeather(obj) {
         ${obj.days[3].icon}
         `);
 }
+
+async function getCity() {
+  const ipData = await fetch('https://ipinfo.io/json?token=d0371dd1f9210d');
+  const jsonData = await ipData.json();
+
+  return `${jsonData.city} + ${jsonData.country}`;
+}
