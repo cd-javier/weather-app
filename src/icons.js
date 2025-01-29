a1722a3416bb44f3033cd94c8fe895d07f00f709
Javier Quiroga <contact@javierquiroga.com>
2025-01-29 10:07:49 +0000

    Add custom weather icons

--- src/icons.js
+++ src/icons.js
@@ -1,13 +1,12 @@
-import snow from './icons/weather-rainy.svg';
-import rain from './icons/weather-rainy.svg';
-import fog from './icons/weather-rainy.svg';
-import wind from './icons/weather-rainy.svg';
-import cloudy from './icons/weather-rainy.svg';
-import partlyCloudyDay from './icons/weather-rainy.svg';
-import partlyCloudyNight from './icons/weather-rainy.svg';
-import clearDay from './icons/weather-rainy.svg';
-import clearNight from './icons/weather-rainy.svg';
-import loading from './icons/loading.svg';
+import snow from './icons/snow.png';
+import rain from './icons/rain.png';
+import fog from './icons/fog.png';
+import wind from './icons/wind.png';
+import cloudy from './icons/cloudy.png';
+import partlyCloudyDay from './icons/partly-cloudy-day.png';
+import partlyCloudyNight from './icons/partly-cloudy-night.png';
+import clearDay from './icons/clear-day.png';
+import clearNight from './icons/clear-night.png';
 
 const icons = {
   snow: snow,
@@ -19,7 +18,6 @@ const icons = {
   'partly-cloudy-night': partlyCloudyNight,
   'clear-day': clearDay,
   'clear-night': clearNight,
-  loading: loading,
 };
 
 export default icons;
